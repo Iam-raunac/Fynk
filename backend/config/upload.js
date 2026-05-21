c38542873bcf65ecc0caecb73c617aa1894299b9
Iam-raunac <148640740+Iam-raunac@users.noreply.github.com>
2026-05-21 11:38:29 +0530

    fix render env loading

--- backend/config/upload.js
+++ backend/config/upload.js
@@ -2,7 +2,9 @@ import fs from "fs/promises";
 import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 
-dotenv.config({ path: "../.env" });
+dotenv.config();
+
+// dotenv.config({ path: "../.env" });
 
 const hasCloudinaryConfig = () =>
   Boolean(
