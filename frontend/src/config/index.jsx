9a70f3adec887bdd9e83515fabd4d5f2b5a8304a
Iam-raunac <148640740+Iam-raunac@users.noreply.github.com>
2026-05-21 11:59:07 +0530

    updated frontend api url

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -3,7 +3,7 @@ const { default: axios } = require("axios");
 
 
 
-export const BASE_URL = "http://localhost:9090"
+export const BASE_URL = "https://fynk.onrender.com/"
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
